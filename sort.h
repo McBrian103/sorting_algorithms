c7a623eead22ffcd04f1f6ceffbf8e0d96d9f1f0
McBrian103 <96251276+McBrian103@users.noreply.github.com>
2022-07-19 22:26:41 +0300

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,32 @@
+#ifndef SORT_H
+#define SORT_H
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+listint_t *create_listint(const int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void swap(listint_t **head, listint_t *node1, listint_t *node2);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+int split(int *arr, int left, int right, size_t size);
+void sort_alg(int *arr, int left, int right, size_t size);
+
+#endif
